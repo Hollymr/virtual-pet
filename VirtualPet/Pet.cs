d25b2a5e6a7fe6b3c72ae46c867318ce8fdde538
Holly Rhodes <hllyrhodes@gmail.com>
2016-06-24 13:22:38 -0400

    main menu/ pet name

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        public string PetName { get; set; }
+        public int Feed { get; set; }
+        public int Bathroom { get; set; }
+        public int Play { get; set; }
+        public int TakeForSwim { get; set; }
+
+
+        public void MainMenu()
+        {
+            Pet myPet = new Pet();
+
+            Console.WriteLine("\nChoose an option below to interact with " + myPet.PetName + "!");
+            Console.WriteLine("\n1. Feed");
+            Console.WriteLine("2. Waste");
+            Console.WriteLine("3. Play");
+            Console.WriteLine("4. Take for a swim");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -8,8 +8,49 @@ namespace VirtualPet
 {
     class Program
     {
+        /* Meet your new pet Shark
+         * Name your pet
+         * Display
+         *
+         */
+        
+
+
         static void Main(string[] args)
         {
+            Pet myPet = new Pet();
+
+            Console.WriteLine("Welcome, meet your new pet shark!");
+            Console.WriteLine("\nPlease name your pet.");
+            myPet.PetName = Console.ReadLine();
+            myPet.MainMenu();
+
+            
+            //Console.WriteLine("\nChoose an option below to interact with " + myPet.PetName + "!");
+            //Console.WriteLine("\n1. Feed");
+            //Console.WriteLine("2. Waste");
+            //Console.WriteLine("3. Play");
+            //Console.WriteLine("4. Take for a swim");
+
+
+
+
+            while (true)
+            {
+
+                
+
+
+
+
+
+
+
+
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
